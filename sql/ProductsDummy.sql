b60f7af1e2e5a9871affdb88305581aa3fbcb181
Nicky Van den Brande <nicky.vandenbrande@hotmail.com>
2017-10-27 08:49:01 +0200

    Dummyproducten

---
+++ sql/ProductsDummy.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM dbjavaadv.product
+LIMIT 0, 1000
+
+-- Date: 2017-10-27
+
+PASSWORDS: asdff    OR    fdsaa   OR asdfg
+*/
+INSERT INTO `product` (`name`,`price`,`description`,`stock`) VALUES ('pakket cola','13','24 blikje','5');
+INSERT INTO `product` (`name`,`price`,`description`,`stock`) VALUES ('pakket fanta','13','24 blikje','5');
+INSERT INTO `product` (`name`,`price`,`description`,`stock`) VALUES ('pakket sprite','13','24 blikje','5');
+INSERT INTO `product` (`name`,`price`,`description`,`stock`) VALUES ('pakket cherry cola','15','24 blikje','5');
+INSERT INTO `product` (`name`,`price`,`description`,`stock`) VALUES ('pakket schweppes','16','24 blikje','5');
+
