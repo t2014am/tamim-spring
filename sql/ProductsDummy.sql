8e3c0ae4f73bd26781611ebfba1bfc59af62b644
Erik <erik@Hackbook-Pro-Z50.local>
2017-12-04 21:06:44 +0100

    Product & Categorydummy update

--- sql/ProductsDummy.sql
+++ sql/ProductsDummy.sql
@@ -2,10 +2,17 @@
 -- Query: SELECT * FROM dbjavaadv.Product
 LIMIT 0, 1000
 
--- Date: 2017-11-16 15:26
+-- Date: 2017-12-04 21:05
 */
-INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (1,1,'24 blikje','fanta.png','fanta',13,5);
-INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (2,1,'24 blikje','sprite.png','sprite',13,5);
-INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (3,1,'24 blikje','cherry.png','cherry cola',15,5);
-INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (4,1,'24 blikje','schweppes.png','schweppes',16,5);
-INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (5,2,'10 pakken koffie','douwe.png','Douwe Egberts',10,10);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (1,1,'24 blikje','images/products/fanta.png','Fanta',13,5);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (2,1,'24 blikje','images/products/sprite.png','Sprite',13,5);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (3,1,'24 blikje','images/products/cherry.png','Coca Cola Cherry ',15,5);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (4,1,'24 blikje','images/products/schweppes.png','Schweppes',16,5);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (5,2,'10 pakken koffie','images/products/douwe.png','Douwe Egberts',10,10);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (6,4,'Zware bier joh','images/products/duvel.png','Duvel',10,10);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (7,1,'Just Sauce. Raw Sauce','images/products/bigshaq.png','No Ketchup',10,10);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (16,1,'24 Blikjes','images/products/fristi.png','Fristi',10,5);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (17,3,'1 Fles','images/products/walker.png','Johnny Walker',10,5);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (18,4,'1 bak van 24 flesjes van 25 cl','images/products/jupiler.png','Jupiler',10,10);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (19,1,'24 blikjes regular','images/products/colareg.png','Coca Cola',10,10);
+INSERT INTO `Product` (`id`,`categoryId`,`description`,`image`,`name`,`price`,`stock`) VALUES (20,4,'24 blikjes','images/products/maes.png','Maes',10,10);
